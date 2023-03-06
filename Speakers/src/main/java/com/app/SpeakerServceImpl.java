09badc58026e584d910825390f047eefacfb2f15
Pawan <kumarpawanm8085@gmail.com>
2023-03-06 11:47:08 +0530

    some modification

--- Speakers/src/main/java/com/app/SpeakerServceImpl.java
+++ Speakers/src/main/java/com/app/SpeakerServceImpl.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-
+import org.springframework.stereotype.Service;
+@Service
 public class SpeakerServceImpl implements SpeakerService{
 	
 	private SpeakerRepository speakerRepository;
